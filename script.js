50a05f496b863230fa5af365d789915f140a23d6
Guilherme Salvador <guilherme.s.2@outlook.com>
2026-05-06 13:45:55 -0300

    Add sketchpad grid structure

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+const container = document.querySelector("#container");
+
+const grid = 16;
+document.documentElement.style.setProperty("--grid-size", grid);
+
+for (let i = 0; i < grid * grid; i++) {
+  const cell = document.createElement("div");
+  cell.id = `cell-${i + 1}`;
+  cell.classList.toggle("grid-cell");
+  container.append(cell);
+}
