03b9dcfda1a5777f7de42ae7f61cc603d03cd726
Guilherme Salvador <guilherme.s.2@outlook.com>
2026-05-06 14:17:44 -0300

    Add paint logic to the sketchpad

--- script.js
+++ script.js
@@ -6,6 +6,27 @@ document.documentElement.style.setProperty("--grid-size", grid);
 for (let i = 0; i < grid * grid; i++) {
   const cell = document.createElement("div");
   cell.id = `cell-${i + 1}`;
-  cell.classList.toggle("grid-cell");
-  container.append(cell);
+  cell.classList.add("grid-cell");
+  container.appendChild(cell);
 }
+
+let isDrawing = false;
+
+function handleMouseOver(e) {
+  if (!isDrawing) return;
+
+  if (e.target.classList.contains("grid-cell")) {
+    e.target.style.backgroundColor = "black";
+  }
+}
+
+container.addEventListener("mousedown", (event) => {
+  isDrawing = true;
+
+  event.target.style.backgroundColor = "black";
+  container.addEventListener("mouseover", (event) => handleMouseOver(event));
+});
+
+container.addEventListener("mouseup", () => {
+  isDrawing = false;
+});
