8646beb5f9b8c77502cf32c6f8e24d5dea986404
Guilherme Salvador <guilherme.s.2@outlook.com>
2026-05-06 16:22:00 -0300

    Add button to alter grid size

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const container = document.querySelector("#container");
 
-const grid = 16;
-document.documentElement.style.setProperty("--grid-size", grid);
+let grid = 16;
+defineGridSize(16);
 
-for (let i = 0; i < grid * grid; i++) {
-  const cell = document.createElement("div");
-  cell.id = `cell-${i + 1}`;
-  cell.classList.add("grid-cell");
-  container.appendChild(cell);
+function defineGridSize(size) {
+  grid = size;
+  document.documentElement.style.setProperty("--grid-size", size);
+  createGrid();
+}
+
+function createGrid() {
+  container.innerHTML = "";
+
+  for (let i = 0; i < grid * grid; i++) {
+    const cell = document.createElement("div");
+    cell.id = `cell-${i + 1}`;
+    cell.classList.add("grid-cell");
+    container.appendChild(cell);
+  }
 }
 
 let isDrawing = false;
@@ -29,4 +39,20 @@ container.addEventListener("mousedown", (event) => {
 
 container.addEventListener("mouseup", () => {
   isDrawing = false;
+});
+
+const gridBtn = document.querySelector("#grid-btn");
+gridBtn.addEventListener("click", () => {
+  let input;
+
+  do {
+    input = window.prompt(
+      "Enter a number to the desired grid size. (Min 1 - Max 100)",
+    );
+
+    // se cancelar, sai
+    if (input === null) return;
+  } while (input === "" || isNaN(input) || input < 1 || input > 100);
+
+  defineGridSize(input);
 });
